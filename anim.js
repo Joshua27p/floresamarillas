55a0f3e20116a10a8f239213f2a05e1b78a1a336
joshua <josh27p@gmail.com>
2024-09-22 15:08:03 -0500

    feliz cumpleaños ferxxo

---
+++ anim.js
@@ -0,0 +1,53 @@
+// Sincronizar las letras con la canción
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+
+// Array de objetos que contiene cada línea y su tiempo de aparición en segundos
+var lyricsData = [
+  { text: "Estás Invitado a mi cumple", time: 1 },
+  { text: "No puedes faltar", time: 3 },
+  { text: "Es este viernes 27 de septiembre", time: 7 },
+  { text: "Habrá SOLO ron, PERO TRAE MÁS", time: 11 },
+  { text: "HARTO PERREO Y CHOCOLATE", time: 14 },
+  { text: "EL AUFIT CON CAMISETA DE TU EQUIPO FAVORITO", time: 18 },
+  { text: "No te portes mal.", time: 21 },
+  { text: "SIN MÍ GAA.", time: 24 },
+];
+
+// Animar las letras
+function updateLyrics() {
+  var time = Math.floor(audio.currentTime);
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 6
+  );
+
+  if (currentLine) {
+    // Calcula la opacidad basada en el tiempo en la línea actual
+    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+
+    // Aplica el efecto de aparición
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+  } else {
+    // Restablece la opacidad y el contenido si no hay una línea actual
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+  }
+}
+
+setInterval(updateLyrics, 1000);
+
+//funcion titulo
+// Función para ocultar el título después de 216 segundos
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 1s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); // Espera 3 segundos antes de ocultar completamente
+}
+
+// Llama a la función después de 216 segundos (216,000 milisegundos)
+setTimeout(ocultarTitulo, 216000);
